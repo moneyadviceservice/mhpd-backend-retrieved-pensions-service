ae3a47bb5882f5feb539b49082183c0f012d2807
levi.butler <levi.butler@solirius.com>
2025-09-18 13:42:01 +0000

    Merged PR 7237: 40058 Configure newtonsoft-json-schema-license
    Configure newtonsoft-json-schema-license
    
    Related work items: #40058

--- app/RetrievedPensionsRecordFunction/Program.cs
+++ app/RetrievedPensionsRecordFunction/Program.cs
@@ -22,7 +22,7 @@ var host = new HostBuilder()
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         services.AddMhpdCosmosDb(hostContext.Configuration);
-        services.AddMhpdUtilities();
+        services.AddMhpdUtilities(hostContext.Configuration);
         services.AddMhpdServiceBusTools();
         services.AddScoped<IPensionRecordValidator, PensionRecordValidator>();
         services.AddScoped<IPensionRecordRepository, PensionRecordRepository>();
