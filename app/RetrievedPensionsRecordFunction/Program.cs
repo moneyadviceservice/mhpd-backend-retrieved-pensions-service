9e29e657a0e6c3f8d30ea29070dc3be9dc0e89f3
levi.butler <levi.butler@solirius.com>
2026-03-03 14:19:29 +0000

    Revert Function Application Host Builder When Deployed
    Related work items: #45794

--- app/RetrievedPensionsRecordFunction/Program.cs
+++ app/RetrievedPensionsRecordFunction/Program.cs
@@ -9,64 +9,90 @@ using Microsoft.OpenApi;
 using RetrievedPensionsRecordFunction.Repository;
 using System.Reflection;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = CreateHost();
+await host.RunAsync();
 
-builder.Configuration
-    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
-
-builder.ConfigureFunctionsWebApplication();
-
-if (!string.IsNullOrEmpty(builder.Configuration.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING")))
+IHost CreateHost()
 {
-    builder.Services.AddApplicationInsightsTelemetryWorkerService();
+    var statrupConfiguration= new ConfigurationBuilder().AddEnvironmentVariables().Build();
+    var useFunctionsApplicationBuilder = statrupConfiguration.GetValue("USE_FUNCTIONSAPPLICATION_BUILDER", false);
+    Console.WriteLine($"USE_FUNCTIONSAPPLICATION_BUILDER: {useFunctionsApplicationBuilder}");
+    if (useFunctionsApplicationBuilder)
+    {
+        // LB: Due to a performance issue where all http requests are an order of magnitude slower, we are not currently able to fully support the use of FunctionsApplication
+        // However generating swagger doc is no longer maintained by the legacy package and must be done using the new builder.
+        // For the moment, FunctionsApplication is only used by the build pipeline. This should be revisited in the future when the performance issue is resolved and we can remove the legacy builder.
+        var builder = FunctionsApplication.CreateBuilder([]);
+        builder.ConfigureFunctionsWebApplication();
+        RegisterServices(builder.Services, builder.Configuration);
+        builder.ConfigureAspNetCoreMvcIntegration(mvcBuilder =>
+                    {
+                        mvcBuilder.AddMvcOptions(mvcOptions => { });
+                    })
+                .UseAspNetCoreMiddleware(app =>
+                    {
+                        app.UseFunctionSwaggerUI();
+                        app.UseSwagger(c => c.OpenApiVersion = OpenApiSpecVersion.OpenApi2_0);
+                        app.UseSwaggerUI();
+                    });
+
+        return builder.Build();
+    }
+    else
+    {
+        return new HostBuilder()
+            .ConfigureFunctionsWebApplication()
+            .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+                })
+            .ConfigureServices((hostContext, services) =>
+                {
+                    RegisterServices(services, hostContext.Configuration);
+                })
+            .Build();
+
+    }
 }
 
-builder.Services.AddMhpdCosmosDb(builder.Configuration);
-builder.Services.AddMhpdUtilities(builder.Configuration);
-builder.Services.AddMhpdServiceBusTools();
-builder.Services.AddTransformServices();
-builder.Services.AddScoped<IPensionRecordRepository, PensionRecordRepository>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+void RegisterServices(IServiceCollection services, IConfiguration configuration)
 {
-    c.EnableAnnotations();
-    c.SwaggerDoc("v1", new OpenApiInfo
+    if (!string.IsNullOrEmpty(configuration.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING")))
     {
-        Title = "MaPS Retrieved Pension Records",
-        Description = "This service allows a client to retrieve retrieved pension records related to a user session",
-        Contact = new OpenApiContact
+        services.AddApplicationInsightsTelemetryWorkerService();
+    }
+
+    services.AddMhpdCosmosDb(configuration);
+    services.AddMhpdUtilities(configuration);
+    services.AddMhpdServiceBusTools();
+    services.AddTransformServices();
+    services.AddScoped<IPensionRecordRepository, PensionRecordRepository>();
+
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.EnableAnnotations();
+        c.SwaggerDoc("v1", new OpenApiInfo
         {
-            Name = "General Enquires",
-            Email = "contact@maps.org.uk",
-            Url = new Uri("https://maps.org.uk/en/about-us/contact-us")
-        },
-        License = new OpenApiLicense
+            Title = "MaPS Retrieved Pension Records",
+            Description = "This service allows a client to retrieve retrieved pension records related to a user session",
+            Contact = new OpenApiContact
+            {
+                Name = "General Enquires",
+                Email = "contact@maps.org.uk",
+                Url = new Uri("https://maps.org.uk/en/about-us/contact-us")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Government API License",
+                Url = new Uri("https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/")
+            },
+        });
+        c.DocumentFilter<PensionDataOpenApiFilter>(Assembly.GetExecutingAssembly());
+        c.DocumentFilter<OpenApiParameterFilter>(Assembly.GetExecutingAssembly());
+        c.AddServer(new OpenApiServer
         {
-            Name = "Government API License",
-            Url = new Uri("https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/")
-        },
+            Url = configuration.GetValue<string>("OpenApiServerUrl") ?? "http://localhost:7289"
+        });
     });
-    c.DocumentFilter<PensionDataOpenApiFilter>(Assembly.GetExecutingAssembly());
-    c.DocumentFilter<OpenApiParameterFilter>(Assembly.GetExecutingAssembly());
-    c.AddServer(new OpenApiServer
-    {
-        Url = builder.Configuration.GetValue<string>("OpenApiServerUrl") ?? "http://localhost:7289"
-    });
-});
-
-builder
-    .ConfigureAspNetCoreMvcIntegration(mvcBuilder =>
-    {
-        mvcBuilder.AddMvcOptions(mvcOptions => { });
-    })
-    .UseAspNetCoreMiddleware(app =>
-    {
-        app.UseFunctionSwaggerUI();
-
-        app.UseSwagger(c => c.OpenApiVersion = OpenApiSpecVersion.OpenApi2_0);
-        app.UseSwaggerUI();
-    });
-
-var app = builder.Build();
-await app.RunAsync();
+}
