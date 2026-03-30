3b8e872bf7172f82b8e370a83e1eb405af76fab4
Fola Sonoiki <fola.sonoiki@solirius.com>
2026-03-30 13:27:27 +0000

    47923-Multiplicity-Pension-Detail
    Applied Common package for McCloud support
    
    Related work items: #47355, #47356, #47923

--- app/RetrievedPensionsRecordFunction/RetrievedPensionsFunction.cs
+++ app/RetrievedPensionsRecordFunction/RetrievedPensionsFunction.cs
@@ -154,7 +154,7 @@ public class RetrievedPensionsFunction(ILogger<RetrievedPensionsFunction> logger
 
     private static string GetIncome(JsonNode? resultNode)
     {
-        return GetArrangementProperty(resultNode, PensionConstants.HasIncome, "false", "false");
+        return GetArrangementProperty(resultNode, PensionConstants.HasIncome, Boolean.FalseString, Boolean.FalseString);
     }
 
     private static string GetAdministrator(JsonNode? resultNode)
@@ -217,7 +217,7 @@ public class RetrievedPensionsFunction(ILogger<RetrievedPensionsFunction> logger
             AssetId = Guid.NewGuid().ToString(),
             Category = Category.Error,
             SchemeName = Constants.UnkonwnPensionScheme,
-            HasIncome = "false",
+            HasIncome = Boolean.FalseString,
             Administrator = Constants.UnkonwnAdministrator,
             RetrievalResult = JsonSerializer.Deserialize<dynamic>(error)
         };
